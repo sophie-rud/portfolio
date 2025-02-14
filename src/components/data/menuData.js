577d2161e27c6ba285cd0bee380a4b1000409509
sophie-rud <sophie.rudio@lapiscine.pro>
2025-02-14 01:11:59 +0100

    Adjustments in home & about sections

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,10 +1,10 @@
 export const mainMenu = [
-     {link:"#", content:"Home", icon:"lucide:home"},
-     {link:"#about", content:"À propos", icon:"octicon:person-24"},
+     {link:"#", content:"Accueil", icon:"lucide:home"},
+     {link:"#about", content:"Présentation", icon:"octicon:person-24"},
      {link:"#skills", content:"Compétences", icon:"tabler:list-check"},
      {link:"#background", content:"Parcours", icon:"pepicons-print:cv"},
      {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
-     {link:"#blog", content:"Blog", icon:"mdi:blog"}
+     // {link:"#blog", content:"Blog", icon:"mdi:blog"}
 ]
 export const menuBlog = [
      {link:"/", content:"Home", icon:"lucide:home"},

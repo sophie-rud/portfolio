16e7a4a5726c3f7e17f77fa937d4bd9a00ad9a44
sophie-rud <sophie.rudio@lapiscine.pro>
2025-02-14 18:12:53 +0100

    Addition of the summary section

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,9 +1,10 @@
 export const mainMenu = [
-     {link:"#", content:"Accueil", icon:"lucide:home"},
+     // {link:"#", content:"Accueil", icon:"lucide:home"},
      {link:"#about", content:"Présentation", icon:"octicon:person-24"},
      {link:"#skills", content:"Compétences", icon:"tabler:list-check"},
      {link:"#background", content:"Parcours", icon:"pepicons-print:cv"},
      {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
+     {link:"#summary", content:"Contact & réseaux", icon:"ic:twotone-mail"},
      // {link:"#blog", content:"Blog", icon:"mdi:blog"}
 ]
 export const menuBlog = [

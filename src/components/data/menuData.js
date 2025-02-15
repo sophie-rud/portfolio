d73f468924e0b6442e052fc018b1bd550ff1e4b7
sophie-rud <sophie.rudio@lapiscine.pro>
2025-02-15 16:00:53 +0100

    Responsive implementation

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -3,8 +3,8 @@ export const mainMenu = [
      {link:"#about", content:"Présentation", icon:"octicon:person-24"},
      {link:"#skills", content:"Compétences", icon:"tabler:list-check"},
      {link:"#background", content:"Parcours", icon:"pepicons-print:cv"},
-     {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
-     {link:"#summary", content:"Contact & réseaux", icon:"ic:twotone-mail"},
+     {link:"#portafolio", content:"Portfolio", icon:"dashicons:portfolio"},
+     {link:"#summary", content:"Contact", icon:"ic:twotone-mail"},
      // {link:"#blog", content:"Blog", icon:"mdi:blog"}
 ]
 export const menuBlog = [
